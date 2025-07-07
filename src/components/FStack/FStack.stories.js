d5999c934afdbf130e2ae4b43c74e0533204d1ba
Dmitry Dobrynin <dd@tovarisch.engineer>
2025-07-07 16:15:21 +0200

    ✨ (FStack): Added FStack component that arranges children in a stack

---
+++ src/components/FStack/FStack.stories.js
@@ -0,0 +1,70 @@
+import FStack from '.';
+import FInput from '@/components/forms/FInput';
+import FButton from '@/components/FButton';
+import FIcon from '@/components/FIcon';
+
+
+const usage = `
+### Usage
+
+Import the component:
+
+\`\`\`js
+import { FStack } from 'futility-ui'
+// or
+import FStack from 'futility-ui/FStack'
+\`\`\`
+
+Use it in your template:
+
+\`\`\`html
+<FStack>
+	<FButton>Generate</FButton>
+	<FInput placeholder="API key" />
+	<FButton icon="square" color="white" ><FIcon name="clipboard_outline" /></FButton>
+	<FButton color="gray-200" >Revoke</FButton>
+</FStack>
+\`\`\`
+
+That's it!
+`;
+
+export default {
+	title: 'FStack',
+	component: FStack,
+	tags: [ 'autodocs' ],
+	parameters: {
+		layout: 'centered',
+		docs: {
+			description: {
+				component: usage,
+			},
+		}
+	},
+	argTypes: {
+		default: {
+			control: { type: null },
+			table: {
+				category: 'slots',
+				type: { summary: 'vnode' },
+				defaultValue: { summary: null },
+			},
+		},
+	},
+	render: (args, { argTypes, updateArgs }) => ({
+		name: 'FIconDiversityStory',
+		props: Object.keys(argTypes),
+		components: { FStack, FButton, FInput, FIcon },
+		setup() { return { args }; },
+		template: `
+<FStack>
+	<FButton>Generate</FButton>
+	<FInput placeholder="API key" />
+	<FButton icon="square" color="white" ><FIcon name="clipboard_outline" /></FButton>
+	<FButton color="gray-200" >Revoke</FButton>
+</FStack>
+`,
+	}),
+};
+
+export const Default = {};
