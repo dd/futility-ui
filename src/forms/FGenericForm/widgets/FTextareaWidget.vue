58f57cdc4150b0865b407666ab1a45a0fd79dd77
Dmitriy Dobrynin <dd@tovarisch.engineer>
2026-05-03 17:06:07 +0200

    ✨ (FGenericForm): Add FTextarea widget

---
+++ src/forms/FGenericForm/widgets/FTextareaWidget.vue
@@ -0,0 +1,42 @@
+<template>
+	<FFormRow
+		:id="id"
+		:layout="layout"
+		:size="size"
+		:error-text="errorText"
+		:error-highlight="error"
+		:disabled="disabled"
+	>
+		<template v-if="meta.label" #label>{{ meta.label }}</template>
+		<FTextarea
+			v-model="value"
+			:id="id"
+			:name="name"
+			:error="error"
+			:disabled="disabled"
+			:readonly="readonly"
+			:required="required"
+			:size="size"
+			v-bind="attrs"
+		/>
+		<template v-if="meta.helpText" #help>{{ meta.helpText }}</template>
+	</FFormRow>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+import FTextarea from '@/forms/FTextarea';
+import FFormRow from '@/forms/FFormRow';
+import { useWidget, useWidgetField, WIDGET_PROPS, WIDGET_EMITS } from '../useWidget';
+
+defineOptions({ name: 'FTextareaWidget' });
+const model = defineModel({ type: Object });
+const props = defineProps({ ...WIDGET_PROPS });
+defineEmits(WIDGET_EMITS);
+
+const { fields } = useWidget(model, props);
+const { id, name, value, attrs, disabled, readonly, required, error, errorText } = useWidgetField(
+	model, props, computed(() => fields.value[0])
+);
+</script>
